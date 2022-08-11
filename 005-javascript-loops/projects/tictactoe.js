9e4864d35940696a8012ea518f0aebaea1ac05e9
A.correa <Mokimbo1@gmail.com>
2022-08-11 12:47:05 -0400

    update to tictactoe

--- 005-javascript-loops/projects/tictactoe.js
+++ 005-javascript-loops/projects/tictactoe.js
@@ -1,10 +1,8 @@
 const prompt = require('prompt-sync')();
 
-
-
-
-
-
+let ro = ' '
+let co = ' '
+let turn = ' '
 let player1 = ' '
 let player2 = ' '
 let start = Math.random();
@@ -17,7 +15,7 @@ let board = [
     [' ',' ',' '],
     [' ',' ',' ']
     ]
-console.log('      0    1    2\n 0',board[0],'\n 1',board[1],'\n 2',board[2],);
+    console.log('      0    1    2\n 1',board[0],'\n 2',board[1],'\n 3',board[2],);
 do{
     game = 'continue'
     if (restart === 'y'){
@@ -28,81 +26,72 @@ do{
 
 // pick who goes first
     start = Math.random()
-    if (start < 0.5) {
-    console.log(`${player1} goes first`);
-    } 
-    else {
-    console.log(`${player2} goes first`);
-    }
+        if (start < 0.5) {
+        console.log(`${player1} goes first\n`);
+        turn = player1
+        } 
+        else {
+        console.log(`${player2} goes first\n`);
+        turn = player2
+        }
     restart = 'n'
     board = [
         [' ',' ',' '],
         [' ',' ',' '],
         [' ',' ',' ']
         ]
-   console.log(`Game Start!`)
+   console.log(`Game Start!\n`)
     }
-    
-// Check for win condition & possible restart?
+    while (game !== 'end'){
+// Check for win condition 'O' & possible restart?
         if (board[0][0] === 'O' && board[0][1] === 'O' && board[0][2] === 'O'){
-        console.log(`${player2} wins!\n`)
-        answer = prompt(`Would you like to play again?(y/n) `)
-            if (answer = 'y'){
-                restart = 'y' 
-            }
-            else {
-                restart = 'n'
-                game = 'end'
-            }
+            console.log(`${player2} wins!\n`)
+            answer = prompt(`Would you like to play again?(y/n) `)
+                if (answer === 'y'){
+                    restart = 'y' 
+                } 
+                else {
+                    restart = 'n'
+                    game = 'end'
+                }
         }
-        else {
-// Player1's turns
-        while (start < 0.5){
-            console.log(`${player1}'s turn\n`)
-            let a = prompt(`What column you like to place you're X?(ex:the top would be 0) `)
-            let b = prompt(`What row you like to place you're X?(ex: the left would be 0) `)
-            if (board[a][b] !== 'X'){
-                if(board[a][b] !== 'O'){
-                    board[a][b] = 'X'
-                    console.log('      0    1    2\n 0',board[0],'\n 1',board[1],'\n 2',board[2],);
-// Continue to next players turn                
-                    start = start+1
-                    
+// Check for win condition 'X' & possible restart?
+        else if (board[0][0] === 'X' && board[0][1] === 'X' && board[0][2] === 'X'){
+            console.log(`${player1} wins!`)
+            answer = prompt(`Would you like to play again?(y/n) `)
+                if (answer === 'y'){
+                    restart = 'y'
                 }
                 else {
-                    console.log(`There is a mark already there. Try again.`)
+                    restart = 'n'
+                    game = 'end'
                 }
+        }
+        else {
+// Player's turns
+            console.log(`${turn}'s turn\n`)
+            if (turn === player1){
+                ro = prompt('What row you like to place you\'re X?(ex: the top would be 1) ')
+                co = prompt('What column you like to place you\'re X?(ex:the left would be 0) ')
             }
             else {
-            console.log(`There is a mark already there. Try again.`)
-        }
-        }
-        }
-// Check for win condition
-        if (board[0][0] === 'X' && board[0][1] === 'X' && board[0][2] === 'X'){
-        console.log(`${player1} wins!`)
-        answer = prompt(`Would you like to play again?(y/n) `)
-            if (answer = 'y'){
-                restart = 'y'
+                ro = prompt('What row you like to place you\'re O?(ex: the top would be 1) ')
+                co = prompt('What column you like to place you\'re O?(ex:the left would be 0) ')   
             }
-            else {
-                restart = 'n'
-                game = 'end'
-            }
-        }
-        else{
-// Player2's turns
-        while (start > 0.5){
-            console.log(`${player2}'s turn\n`)
-            let a = prompt(`What column you like to place you're O?(ex: the top would be 0) `)
-            let b = prompt(`What row you like to place you're O?(ex: the left would be 0) `)
-            if (board[a][b] !== 'X'){
-                if (board[a][b] !== 'O'){
-                    board[a][b] = 'O'
-                    console.log('      0    1    2\n 0',board[0],'\n 1',board[1],'\n 2',board[2],);
+            if (board[(ro-1)][co] !== 'X'){
+                if(board[(ro-1)][co] !== 'O'){
+                    if(turn === player1){
+                        board[(ro-1)][co] = 'X'
+                        console.log('      0    1    2\n 1',board[0],'\n 2',board[1],'\n 3',board[2],);
 // Continue to next players turn                
-                    start = start-1
-                        
+                        turn = player2
+                    }
+                    else {
+                        board[(ro-1)][co] = 'O'
+                        console.log('      0    1    2\n 1',board[0],'\n 2',board[1],'\n 3',board[2],);
+// Continue to next players turn                
+                        turn = player1
+                    }
                 }
                 else {
                     console.log(`There is a mark already there. Try again.`)
@@ -111,6 +100,6 @@ do{
             else {
                 console.log(`There is a mark already there. Try again.`)
             }
-        }
-        }
+    }
+}
 }while (game === 'continue')
